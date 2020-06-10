33bf860aba34c121c2d769f154f9fce16756c579
amar antil <amartt2174@gmail.com>
2020-06-10 17:50:02 +0530

    first commit

---
+++ groups/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.utils.text import slugify
+
+# Create your models here.
+import misaka
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+from django import template
+register = template.Library()
+
+class Group(models.Model):
+    pass
+
+class GroupMember(models.Model):
+    group = models.ForeignKey(Group,related_name='memberships')
+    user = models.ForeignKey(User,related_name='user_groups')
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        unique_together = ('group','user')
